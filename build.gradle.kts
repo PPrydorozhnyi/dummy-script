0b34dac98bae57fe936624df36ad317b5348280f
Petro Prydorozhnyi <petro.p@delasport.com>
2024-03-27 14:10:21 +0200

    script

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.diffplug.spotless") version "6.25.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+
+spotless {
+    java {
+        target("src/*/java/**/*.java")
+
+        palantirJavaFormat()
+        removeUnusedImports()
+        importOrder()
+        formatAnnotations()
+
+        custom("Refuse wildcard imports") {
+            // Spotless can't resolve wildcard imports itself.
+            // This will require the developers themselves to adhere to best practices.
+            val importPattern = "import .*\\*;".toRegex()
+
+            if (importPattern.containsMatchIn(it)) {
+                throw AssertionError("Do not use wildcard imports. 'spotlessApply' cannot resolve this issue.")
+            } else {
+                it
+            }
+        }
+    }
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktlint()
+    }
+}
